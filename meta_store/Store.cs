5ed5a488751b9585b2a552a25466621ccb43a1fb
Phat <damphat@gmail.com>
2022-05-23 18:01:20 +0700

    Add Store implements and tests

---
+++ meta_store/Store.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace meta_store
+{
+    public class Store
+    {
+        public Store Parent => parent;
+
+        public Store Root
+        {
+            get
+            {
+                var ret = this;
+
+                while (ret.parent != null)
+                {
+                    ret = ret.parent;
+                }
+                return ret;
+            }
+        }
+
+        public string Path
+        {
+            get
+            {
+                var store = this;
+                var path = store.key ?? "";
+
+                store = store.parent;
+
+                while (store != null)
+                {
+                    if (!string.IsNullOrEmpty(store.key))
+                    {
+                        path = store.key + '/' + path;
+                    }
+
+                    store = store.parent;
+                }
+
+                return path;
+            }
+        }
+
+        public string Key => key;
+
+        private object state;
+        private Store parent;
+        private string key;
+        private Dictionary<string, Store> children;
+        private HashSet<Action<object>> listeners;
+        private HashSet<Action<object>> childListeners;
+
+        public Store At1(string key)
+        {
+            children ??= new Dictionary<string, Store>();
+
+            return children.TryGetValue(key, out var value)
+                ? value
+                : children[key] = new Store
+                {
+                    key = key,
+                    parent = this,
+                };
+        }
+
+        public Store At(string path)
+        {
+            return Sigo.ToKeys(path).Aggregate(this, (current, k) => current.At1(k));
+        }
+
+        public object Get1(string key)
+        {
+            var ret = Sigo.Get1(this.state, key);
+            Sigo.Freeze(ret);
+            return ret;
+        }
+
+        public object Get(string path)
+        {
+            var ret = Sigo.Get(this.state, path);
+            Sigo.Freeze(ret);
+            return ret;
+        }
+
+        public object Get()
+        {
+            Sigo.Freeze(state);
+            return state;
+        }
+
+        public void Set1(string key, object value)
+        {
+            Sigo.Freeze(value);
+            this.state = Sigo.Set1(state, key, value);
+        }
+
+        public void Set(string path, object value)
+        {
+            Sigo.Freeze(value);
+            this.state = Sigo.Set(state, path, value);
+        }
+
+        // tương đương với root.Set(path, value)
+        public void Set(object value)
+        {
+            Sigo.Freeze(value);
+
+            if (this.state != value)
+            {
+                this.state = value;
+                SetDown();
+                SetUp();
+            }
+        }
+
+        void SetUp()
+        {
+            if (this.children == null) return;
+            foreach (var child in children)
+            {
+                var v = Sigo.Get1(this.state, child.Key);
+                if (child.Value.state != v)
+                {
+                    child.Value.state = v;
+                    child.Value.SetUp();
+
+                }
+            }
+        }
+
+        public void AddListener(Action<object> action)
+        {
+            // TODO send first value?
+            // TODO numberOfListenerInChildren
+            // TODO unsubcriber
+            // TODO allow duplication
+            listeners.Add(action);
+        }
+
+        public void RemoveListener(Action<object> action)
+        {
+            listeners.Remove(action);
+        }
+
+        void SetDown()
+        {
+            var p = this.parent;
+            if (p != null)
+            {
+                p.state = Sigo.Set1(p.state, key, state);
+                p.SetDown();
+            }
+        }
+    }
+}
---
+++ meta_store_tests/StoreTests.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using meta_store;
+using Microsoft.VisualBasic;
+using NUnit.Framework;
+
+namespace meta_store_tests
+{
+    public class StoreTests
+    {
+        [Test]
+        public void Root()
+        {
+            var root = new Store();
+            Assert.AreEqual(null, root.Key);
+            Assert.AreEqual(null, root.Parent);
+            Assert.AreEqual(root, root.Root);
+            Assert.AreEqual("", root.Path);
+        }
+
+        [Test]
+        public void Test_name()
+        {
+            var root = new Store();
+            var name = root.At("name");
+            Assert.AreSame(root.At("name"), name);
+
+            Assert.AreEqual("name", name.Key);
+            Assert.AreSame(root, name.Parent);
+            Assert.AreSame(root, name.Root);
+            Assert.AreEqual("name", name.Path);
+        }
+
+        [Test]
+        public void Test_name_first()
+        {
+            var root = new Store();
+            var first = root.At("name/first");
+            Assert.AreSame(root.At("name").At("first"), first);
+
+            Assert.AreEqual("first", first.Key);
+            Assert.AreSame(root.At("name"), first.Parent);
+            Assert.AreSame(root, first.Root);
+            Assert.AreEqual("name/first", first.Path);
+        }
+
+        [Test]
+        public void Test_GetSet_root()
+        {
+            var root = new Store();
+            Assert.AreEqual(null, root.Get());
+
+            root.Set("v");
+            Assert.AreEqual("v", root.Get());
+        }
+
+        [Test]
+        public void Test_GetSet_name()
+        {
+            var root = new Store();
+            var name = root.At("name");
+
+            name.Set("phat");
+
+            Assert.AreEqual("phat", name.Get());
+
+            Assert.AreEqual(Sigo.Create("name", "phat"), root.Get());
+        }
+
+        [Test]
+        public void Test_GetSet_name_first_down()
+        {
+            var root = new Store();
+            var first = root.At("name/first");
+
+            first.Set("phat");
+
+            Assert.AreEqual("phat", first.Get());
+            Assert.AreEqual(Sigo.Create("first", "phat"), first.Parent.Get());
+
+            Assert.AreEqual(Sigo.Create("name", Sigo.Create("first", "phat")), first.Root.Get());
+        }
+
+        [Test]
+        public void Test_GetSet_name_first_up()
+        {
+            var root = new Store();
+            var first = root.At("name/first");
+
+            root.Set(Sigo.Create("name", Sigo.Create("first", "phat")));
+
+            Assert.AreEqual("phat", first.Get());
+            Assert.AreEqual(Sigo.Create("first", "phat"), first.Parent.Get());
+            Assert.AreEqual(Sigo.Create("name", Sigo.Create("first", "phat")), first.Root.Get());
+        }
+
+        [Test]
+        public void Test_listeners()
+        {
+            var root = new Store();
+            root.Set("1");
+            var ret = new List<object>();
+            
+            root.AddListener(v => ret.Add(v));
+
+            Assert.AreEqual(new [] {"1"}, ret);
+        }
+    }
+}
