a4f7f03f9701694762b7d36e800885fa0cd49485
Phat <damphat@gmail.com>
2022-05-23 18:00:13 +0700

    fix: missing file from previous commits

--- meta_store/Sigo.Utils.cs
+++ meta_store/Sigo.Utils.cs
@@ -1,114 +1,115 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
-namespace meta_store;
-
-partial class Sigo
+namespace meta_store
 {
-    public static object Create(params object[] keys)
+
+    partial class Sigo
     {
-        object tree = null;
-        var i = 0;
-        Debug.Assert(keys.Length % 2 == 0);
-        while (i < keys.Length - 1)
+        public static object Create(params object[] keys)
         {
-            var key = keys[i++].ToString();
-            var value = keys[i++];
-            tree = Set1(tree, key, value);
-        }
-
-        return tree;
-    }
-
-    public static void KeyValidate(object key)
-    {
-        if (key == null) throw new ArgumentNullException("key");
-        if (key is string s)
-        {
-            if (string.IsNullOrEmpty(s)) throw new ArgumentException("key is empty");
-
-            if (s.Contains('/')) throw new ArgumentException("key contains '/'");
-        }
-    }
-
-    public static object Set1(object obj, string key, object value)
-    {
-        if (obj is Sigo tree)
-        {
-            var old = Get1(tree, key);
-            if (Equals(old, value)) return tree;
-
-            if (tree.frozen)
+            object tree = null;
+            var i = 0;
+            Debug.Assert(keys.Length % 2 == 0);
+            while (i < keys.Length - 1)
             {
-                var data = new Dictionary<string, object>(tree.data);
-                data[key] = value;
-                return new Sigo
-                {
-                    data = data,
-                    frozen = false
-                };
+                var key = keys[i++].ToString();
+                var value = keys[i++];
+                tree = Set1(tree, key, value);
             }
 
-            tree.data[key] = value;
             return tree;
         }
 
-        return new Sigo
+        public static void KeyValidate(object key)
         {
-            data = new Dictionary<string, object> {{key, value}},
-            frozen = false
-        };
-    }
+            if (key == null) throw new ArgumentNullException("key");
+            if (key is string s)
+            {
+                if (string.IsNullOrEmpty(s)) throw new ArgumentException("key is empty");
 
-    public static object Get1(object obj, string key)
-    {
-        if (obj is Sigo tree) return tree.TryGetValue(key, out var value) ? value : null;
+                if (s.Contains('/')) throw new ArgumentException("key contains '/'");
+            }
+        }
 
-        return null;
-    }
+        public static object Set1(object obj, string key, object value)
+        {
+            if (obj is Sigo tree)
+            {
+                var old = Get1(tree, key);
+                if (Equals(old, value)) return tree;
 
-    public static void Freeze(object obj)
-    {
-        if (obj is Sigo tree)
-            if (!tree.frozen)
+                if (tree.frozen)
+                {
+                    var data = new Dictionary<string, object>(tree.data)
+                    {
+                        [key] = value
+                    };
+                    return new Sigo
+                    {
+                        data = data,
+                        frozen = false
+                    };
+                }
+
+                tree.data[key] = value;
+                return tree;
+            }
+
+            return new Sigo
+            {
+                data = new Dictionary<string, object> {{key, value}},
+                frozen = false
+            };
+        }
+
+        public static object Get1(object obj, string key)
+        {
+            return obj is Sigo tree ? tree.TryGetValue(key, out var value) ? value : null : null;
+        }
+
+        public static void Freeze(object obj)
+        {
+            if (obj is Sigo tree && !tree.frozen)
             {
                 tree.frozen = true;
                 foreach (var v in tree.Values) Freeze(v);
             }
-    }
-
-    public static bool IsFrozen(object obj)
-    {
-        if (obj is Sigo tree) return tree.frozen;
-
-        return true;
-    }
-
-    public static object Set(object obj, string[] keys, int from, object value)
-    {
-        if (from >= keys.Length) return value;
-        var key = keys[from];
-
-        return Set1(obj, key, Set(Get1(obj, key), keys, from + 1, value));
-    }
-
-    public static object Set(object obj, string path, object value)
-    {
-        return Set(obj, ToKeys(path), 0, value);
-    }
-
-    public static string[] ToKeys(string path)
-    {
-        return path.Split('/', StringSplitOptions.RemoveEmptyEntries);
-    }
-
-    public static object Get(object obj, string path)
-    {
-        foreach (var key in ToKeys(path))
-        {
-            if (obj is null) break;
-            obj = Get1(obj, key);
         }
 
-        return obj;
+        public static bool IsFrozen(object obj)
+        {
+            return !(obj is Sigo tree) || tree.frozen;
+        }
+
+        public static object Set(object obj, string[] keys, int from, object value)
+        {
+            if (from >= keys.Length) return value;
+            var key = keys[from];
+
+            return Set1(obj, key, Set(Get1(obj, key), keys, from + 1, value));
+        }
+
+        public static object Set(object obj, string path, object value)
+        {
+            return Set(obj, ToKeys(path), 0, value);
+        }
+
+        public static string[] ToKeys(string path)
+        {
+            return path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static object Get(object obj, string path)
+        {
+            foreach (var key in ToKeys(path))
+            {
+                if (obj is null) break;
+                obj = Get1(obj, key);
+            }
+
+            return obj;
+        }
     }
 }
