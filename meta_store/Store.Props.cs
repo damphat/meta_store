a539568f5f350a000604be6ff395ba8f63bd5ce6
Phat <damphat@gmail.com>
2022-06-19 15:04:32 +0700

    Add store.path and store.root

---
+++ meta_store/Store.Props.cs
@@ -0,0 +1,46 @@
+﻿namespace meta_store
+{
+    public partial class Store
+    {
+        public Store Parent => parent;
+
+        public Store Root
+        {
+            get
+            {
+                var ret = this;
+
+                while (ret.parent != null)
+                {
+                    ret = ret.parent;
+                }
+                return ret;
+            }
+        }
+
+        public string Path
+        {
+            get
+            {
+                var store = this;
+                var path = store.key ?? "";
+
+                store = store.parent;
+
+                while (store != null)
+                {
+                    if (!string.IsNullOrEmpty(store.key))
+                    {
+                        path = store.key + '/' + path;
+                    }
+
+                    store = store.parent;
+                }
+
+                return path;
+            }
+        }
+
+        public string Key => key;
+    }
+}
